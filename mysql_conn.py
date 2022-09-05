f1fcfe559ac9538abc29d35361a5c74d60a8b5dd
entest <hai@entest.io>
2022-09-05 17:08:00 +0000

    fix slow due to random name

--- mysql_conn.py
+++ mysql_conn.py
@@ -4,21 +4,24 @@
 import datetime
 import random
 import json
+import threading
 from concurrent.futures import ThreadPoolExecutor
-from threading import current_thread
+from threading import current_thread, main_thread
 import mysql.connector
 import names
 
 # parameter
 NUM_ROW = 1000
 CHUNK_SIZE = 100
-NUM_THREAD_WORKER = 10
+NUM_THREAD_WORKER = 20
+
 
 with open("config.json", "r", encoding="utf-8") as file:
     config = json.load(file)
 
 
 def get_connect():
+    print(f"get connect {current_thread().name}")
     conn = mysql.connector.connect(
         host=config["ENDPOINT"],
         user=config["USER"],
@@ -104,22 +107,28 @@ def drop_table() -> None:
 
 
 def write_to_table(start_idx = 1):
-    # get connection 
+    # get connection
     conn = get_connect()
+    conn.autocommit = True
+    print(f"thread {current_thread().name} {conn}")
     cursor = conn.cursor()
+    print(f"thread {current_thread().name} {cursor}")
     # time stamp
     now = datetime.datetime.now()
     time_stamp = now.strftime("%Y/%m/%d-%H:%M:%S.%f")
     # employees (id, name, age, time)
+    name = names.get_full_name()
+    print('prepare data ...')
     employees = [
         (
             k,
-            names.get_full_name(),
+            name,
             random.randint(20, 100),
             time_stamp,
         )
         for k in range(start_idx, NUM_ROW+start_idx)
     ]
+    print('data ready ...')
     # tuple
     print('start inserting ...')
     for k in range(NUM_ROW):
@@ -129,16 +138,31 @@ def write_to_table(start_idx = 1):
         if k % CHUNK_SIZE == 0: 
             print(f"{current_thread().name} commit chunk {k // CHUNK_SIZE}")
             conn.commit()
-    # close connection 
+    # close connection
     cursor.close()
     conn.close()
 
 def thread_load_write_test():
     with ThreadPoolExecutor(max_workers=NUM_THREAD_WORKER) as executor:
         for k in range(1, NUM_THREAD_WORKER+1):
+            print(f"submit {k} thread")
             executor.submit(write_to_table, k*NUM_ROW+1)
+
+
+def threading_load_write_test():
+    for k in range(NUM_THREAD_WORKER):
+        t = threading.Thread(target=write_to_table, args=(k*NUM_ROW+1,))
+        t.setDaemon(True)
+        t.start()
+    
+    main_thread = threading.currentThread()
+    for t in threading.enumerate():
+        if t is main_thread:
+            continue
+        t.join()
 
 if __name__ == "__main__":
     create_table()
     thread_load_write_test()
+    # threading_load_write_test()
     # fetch_data()
