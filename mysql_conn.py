77a2982af2378355e4445f4ee3c62a4871a03df9
entest <hai@entest.io>
2022-09-06 02:49:55 +0000

    test

--- mysql_conn.py
+++ mysql_conn.py
@@ -1,18 +1,15 @@
 # haimtran 05 SEP 2022
 # rds mysql iops
 
-import datetime
-import random
 import json
 from concurrent.futures import ThreadPoolExecutor
 from threading import current_thread
 import mysql.connector
-import names
 
 # parameter
 NUM_ROW = 1000
 CHUNK_SIZE = 100
-NUM_THREAD_WORKER = 100
+NUM_THREAD_WORKER = 1
 
 
 with open("config.json", "r", encoding="utf-8") as file:
@@ -50,12 +47,6 @@ def create_table() -> None:
         "PRIMARY KEY (id))"
     )
     cur.execute(employee_table)
-    # show table
-    cur.execute("SHOW TABLES")
-    tables = cur.fetchall()
-    for table in tables:
-        print(f"table: {table}")
-    # close connect
     cur.close()
     conn.close()
 
@@ -86,24 +77,6 @@ def fetch_data():
     return items
 
 
-def drop_table() -> None:
-    """
-    drop table
-    """
-    # get connection 
-    conn = get_connect()
-    # cursor
-    cur = conn.cursor()
-    # drop table if exists
-    drop = "DROP TABLE IF EXISTS employees"
-    # execute
-    cur.execute(drop)
-    # close connection
-    cur.close()
-    conn.close()
-    print("DELETED TABLE")
-
-
 def write_to_table(start_idx = 1):
     # get connection
     conn = get_connect()
@@ -111,34 +84,18 @@ def write_to_table(start_idx = 1):
     print(f"thread {current_thread().name} connect {conn}")
     cursor = conn.cursor()
     print(f"thread {current_thread().name} cursor {cursor}")
-    # time stamp
-    now = datetime.datetime.now()
-    time_stamp = now.strftime("%Y/%m/%d-%H:%M:%S.%f")
-    # employees (id, name, age, time)
-    name = names.get_full_name()
-    print('prepare data ...')
-    employees = [
-        (
-            k,
-            name,
-            random.randint(20, 100),
-            time_stamp,
-        )
-        for k in range(start_idx, NUM_ROW+start_idx)
-    ]
-    print('data ready ...')
-    # tuple
-    print('start inserting ...')
     for k in range(NUM_ROW):
         print(f"{current_thread().name} insert item {k}")
         stmt_insert = "INSERT INTO employees (id, name, age, time) VALUES (%s, %s, %s, %s)"
-        cursor.execute(stmt_insert, employees[k])
+        cursor.execute(stmt_insert, (k+start_idx, 'haitran', 30, '100'))
         if k % CHUNK_SIZE == 0: 
             print(f"{current_thread().name} commit chunk {k // CHUNK_SIZE}")
             conn.commit()
     # close connection
     cursor.close()
     conn.close()
+
+
 
 def thread_load_write_test():
     with ThreadPoolExecutor(max_workers=NUM_THREAD_WORKER) as executor:
@@ -147,6 +104,6 @@ def thread_load_write_test():
             executor.submit(write_to_table, k*NUM_ROW+1)
 
 if __name__ == "__main__":
-    create_table()
-    thread_load_write_test()
-    # fetch_data()
+    # create_table()
+    # thread_load_write_test()
+    fetch_data()
